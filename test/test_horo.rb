c99c1bcff3ff3bd30c8faff47b21295877196897
Aaron Patterson <aaron.patterson@gmail.com>
2010-07-13 14:01:27 -0700

    testing index frameset

--- test/test_horo.rb
+++ test/test_horo.rb
@@ -7,14 +7,17 @@ class TestHoro < Test::Unit::TestCase
   def setup
     $-w = false
     rdoc    = RDoc::RDoc.new
-    @title = 'Ruby on Rails Documentation'
+
+    @title    = 'Ruby on Rails Documentation'
+    @encoding = 'utf-8'
+    @main     = 'railties/README'
 
     rdoc.document [
       '-q',
       '-f', 'horo',
       '--title', @title,
-      '--main', 'railties/README',
-      '--charset', 'utf-8',
+      '--main', @main,
+      '--charset', @encoding,
     ]
     $-w = true
   end
@@ -22,7 +25,11 @@ class TestHoro < Test::Unit::TestCase
   def test_index
     assert_file 'doc/index.html'
     doc = File.open('doc/index.html', 'rb') { |f| Nokogiri.HTML f }
+
     assert_equal @title, doc.css('title').first.content
+    assert_equal @encoding, doc.encoding
+    main_frame = doc.at_css 'frame[name = "docwin"]'
+    assert_equal "#{@main}.html", main_frame['src']
   end
 
   def teardown
